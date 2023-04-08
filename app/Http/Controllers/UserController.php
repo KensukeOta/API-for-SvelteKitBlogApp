7edbcb3fb788b93f60b38fb794e783d808ccdf1f
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2023-04-08 09:08:45 +0900

    ユーザー登録機能のためのルーティングとコントローラーを実装 (#1)

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => ['required', 'between:1,10', 'unique:users'],
+            'email' => ['required', 'email', 'unique:users'],
+            'password' => ['required', 'between:6,30', 'confirmed'],
+        ]);
+
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'password_confirmation' => Hash::make($request->password_confirmation),
+        ]);
+    }
+}
