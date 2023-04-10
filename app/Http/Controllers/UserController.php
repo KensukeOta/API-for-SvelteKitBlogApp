2a20e58a780eb6630dbc67f9eb0d5ec833e6cab1
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2023-04-10 15:44:47 +0900

    LoginRequestを作成し、バリデーションのロジックの一元管理とバリデーションメッセージの日本語化を実装し、ログインロジックを修正 (#3)

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use App\Http\Requests\RegisterRequest;
+use App\Http\Requests\LoginRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
@@ -21,12 +22,9 @@ class UserController extends Controller
         ]);
     }
 
-    public function login(Request $request)
+    public function login(LoginRequest $request)
     {
-        $credentials = $request->validate([
-            'email' => ['required', 'email'],
-            'password' => ['required'],
-        ]);
+        $credentials = $request->only('email', 'password');
 
         if (Auth::attempt($credentials)) {
             $request->session()->regenerate();
