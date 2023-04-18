79a12c3ca857cfdeb509e86d2cbc32e1015fc3a1
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2023-04-18 10:29:22 +0900

    UserモデルとPostモデル間のリレーションシップを実装 (#7)

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use App\Models\Post;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
@@ -43,4 +45,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * ユーザーの記事を取得
+     */
+    public function posts(): HasMany
+    {
+        return $this->hasMany(Post::class);
+    }
 }
