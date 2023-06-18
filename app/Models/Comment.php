34764fa7ab24b497e2b6a43c47e021f8302054ec
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2023-06-18 12:13:19 +0900

    UserモデルとCommentモデル間、PostモデルとCommentモデル間のリレーションシップを実装 (#27)

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -2,8 +2,11 @@
 
 namespace App\Models;
 
+use App\Models\User;
+use App\Models\Post;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Comment extends Model
 {
@@ -14,4 +17,20 @@ class Comment extends Model
         'user_id',
         'post_id',
     ];
+
+    /**
+     * このコメントを所有しているユーザーの取得
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * このコメントを所有している記事の取得
+     */
+    public function post(): BelongsTo
+    {
+        return $this->belongsTo(Post::class);
+    }
 }
