8f142660ef6ec7c2f2b9266bd65b39d5d1ad9162
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2023-07-13 08:49:28 +0900

    PostモデルとTagモデル間のリレーションシップを実装 (#31)

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
+use App\Models\Post;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Tag extends Model
 {
@@ -12,4 +14,9 @@ class Tag extends Model
     protected $fillable = [
         'name',
     ];
+
+    public function posts(): BelongsToMany
+    {
+        return $this->belongsToMany(Post::class);
+    }
 }
